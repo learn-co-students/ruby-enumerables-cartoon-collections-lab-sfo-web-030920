49473aea1b53f590bf29653148dd17e912c46e09
Tisdale Fry <tisdalefry@gmail.com>
2020-03-05 19:56:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(names) # code an argument here
   # Your code here
+  names.each_with_index do |name, idx|
+    puts "#{idx + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(ele_arr)# code an argument here
+  fin_arr = []
+  ele_arr.map do |eles| 
+    fin_arr << (eles.capitalize + '!')
+  end
+  fin_arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.length > 4
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      return cheese
+    end
+  end
+  return nil
 end
+
+
+
+
